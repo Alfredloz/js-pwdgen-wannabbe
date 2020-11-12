3d09cbc31059e244583a2b669fe7a8e10430c99e
Alfredo <alfredogregorio.lozano@gmail.com>
2020-11-12 15:18:24 +0100

    prove con JS
    ho creato il password generator

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+alert('Benvenuto!');
+var userName = prompt ("Inserisci il tuo nome");
+var userLastName = prompt ('Inserisci il tuo cognome');
+var yourColour = prompt ('Inserisci il tuo colore preferito')
+document.getElementById('name').innerHTML = userName + '';
+document.getElementById('lastname').innerHTML = userLastName;
+document.getElementById('password').innerHTML = userName+userLastName+yourColour+1342;
