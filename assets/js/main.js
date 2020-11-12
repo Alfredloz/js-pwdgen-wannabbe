1126261e4337de51daf35cc0e21eb873041a1077
Alfredo <alfredogregorio.lozano@gmail.com>
2020-11-12 17:20:19 +0100

    controllo codice e aggiunta commenti

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,20 @@
 //allerta messaggio
 alert('Benvenuto!');
 
+// prompt name request
 var userName = prompt ("Inserisci il tuo nome");
+
+//prompt last name request
 var userLastName = prompt ('Inserisci il tuo cognome');
+
+//prompt favourite colour request
 var yourColour = prompt ('Inserisci il tuo colore preferito')
+
+//var id name
 document.getElementById('name').innerHTML = userName + '';
+
+//var id last name
 document.getElementById('lastname').innerHTML = userLastName;
+
+//password id generator
 document.getElementById('password').innerHTML = userName+userLastName+yourColour+1342;
