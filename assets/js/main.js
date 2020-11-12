dd30001e96b9a3adbd023ee1d80e48003a10deba
Alfredo <alfredogregorio.lozano@gmail.com>
2020-11-12 16:39:27 +0100

    miglioramento esercizio
    ho aggiunto un pò di css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
+//allerta messaggio
 alert('Benvenuto!');
+
 var userName = prompt ("Inserisci il tuo nome");
 var userLastName = prompt ('Inserisci il tuo cognome');
 var yourColour = prompt ('Inserisci il tuo colore preferito')
